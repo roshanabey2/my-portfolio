6e875869ed299ca8a40715e6668dcdbc283f5d19
Roshan Abeysekera <roshanabey2@gmail.com>
2025-04-29 16:24:44 +0100

    Added playwright and changed icon for splash

--- src/components/Carousel3D.js
+++ src/components/Carousel3D.js
@@ -24,28 +24,34 @@ export default function Carousel3D({ onSelect }) {
           const transform = `rotateY(${offset * 40}deg) translateZ(${offset === 0 ? 200 : 150}px)`;
           return (
             <div key={mode} className={` flex carousel-item ${offset === 0 ? 'active' : ''}`} style={{ transform }}>
-              <button onClick={() => onSelect(modes[index])}>
-                <Image
-                  src={
-                    mode === "minimal"
-                      ? "/minimal__panda_icon.png"
-                      : mode === "showcase"
-                        ? "/showcase_panda_icon.jpg"
-                        : "/dark_panda_icon.png"}
-                  alt={`${mode} mode `}
-                  width={85}
-                  height={85}
-                />
-              </button>
+              <Image
+                src={
+                  mode === "minimal"
+                    ? "/minimal_panda_icon.png"
+                    : mode === "showcase"
+                      ? "/showcase_panda_icon.png"
+                      : "/dark_panda_icon.png"}
+                alt={`${mode} mode `}
+                width={85}
+                height={85}
+              />
 
             </div>
           );
         })}
       </div>
       <div className="controls">
-        <button onClick={rotateRight}> <svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M20 11v2H8v2H6v-2H4v-2h2V9h2v2h12zM10 7H8v2h2V7zm0 0h2V5h-2v2zm0 10H8v-2h2v2zm0 0h2v2h-2v-2z" fill="currentColor" /> </svg> </button>
-        <button width={60} onClick={() => onSelect(modes[index])}> {modes[index]} </button>
-        <button onClick={rotateRight}><svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M4 11v2h12v2h2v-2h2v-2h-2V9h-2v2H4zm10-4h2v2h-2V7zm0 0h-2V5h2v2zm0 10h2v-2h-2v2zm0 0h-2v2h2v-2z" fill="currentColor" /> </svg></button>
+        <button onClick={rotateLeft}>
+          <svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
+            <path d="M20 11v2H8v2H6v-2H4v-2h2V9h2v2h12zM10 7H8v2h2V7zm0 0h2V5h-2v2zm0 10H8v-2h2v2zm0 0h2v2h-2v-2z" fill="currentColor" /> 
+          </svg>
+        </button>
+        <button onClick={() => onSelect(modes[index])}> {modes[index]} </button>
+        <button onClick={rotateRight}>
+          <svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> 
+          <path d="M4 11v2h12v2h2v-2h2v-2h-2V9h-2v2H4zm10-4h2v2h-2V7zm0 0h-2V5h2v2zm0 10h2v-2h-2v2zm0 0h-2v2h2v-2z" fill="currentColor" />
+          </svg>
+        </button>
       </div>
     </div>
   );
