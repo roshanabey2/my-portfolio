f3e4ffbae6719a5e582f4f6f952bbe5dc9e06ee0
Roshan Abeysekera <roshanabey2@gmail.com>
2025-04-29 13:39:29 +0100

    Carousel now has pixelated arrows

--- src/components/Carousel3D.js
+++ src/components/Carousel3D.js
@@ -43,9 +43,9 @@ export default function Carousel3D({ onSelect }) {
         })}
       </div>
       <div className="controls">
-        <button onClick={rotateLeft}><svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M20 11v2H8v2H6v-2H4v-2h2V9h2v2h12zM10 7H8v2h2V7zm0 0h2V5h-2v2zm0 10H8v-2h2v2zm0 0h2v2h-2v-2z" fill="currentColor" /> </svg></button>
-        <button onClick={() => onSelect(modes[index])}> {modes[index]} </button>
-        <button onClick={rotateRight}>⟩</button>
+        <button onClick={rotateRight}> <svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M20 11v2H8v2H6v-2H4v-2h2V9h2v2h12zM10 7H8v2h2V7zm0 0h2V5h-2v2zm0 10H8v-2h2v2zm0 0h2v2h-2v-2z" fill="currentColor" /> </svg> </button>
+        <button width={60} onClick={() => onSelect(modes[index])}> {modes[index]} </button>
+        <button onClick={rotateRight}><svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M4 11v2h12v2h2v-2h2v-2h-2V9h-2v2H4zm10-4h2v2h-2V7zm0 0h-2V5h2v2zm0 10h2v-2h-2v2zm0 0h-2v2h2v-2z" fill="currentColor" /> </svg></button>
       </div>
     </div>
   );
